8e9196ee13633c4873117e434d98d1c641c2051b
Phillipi Valle <ellav.phill@gmail.com>
2024-02-06 00:56:16 -0300

    vercel

--- src/modules/checkout/pages/home.tsx
+++ src/modules/checkout/pages/home.tsx
@@ -4,7 +4,6 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Bem-vindo à Página Home do Caixa</h1>
-      {/* Conteúdo específico da página Home do Dashboard */}
     </div>
   );
 };
