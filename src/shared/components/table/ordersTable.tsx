1429245c86b705c096aa4dbf0f95e56bde932653
Phillipi Valle <ellav.phill@gmail.com>
2024-02-24 01:43:40 -0300

    nova table

--- src/shared/components/table/ordersTable.tsx
+++ src/shared/components/table/ordersTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Order } from '../../interfaces/Order';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 
@@ -7,6 +7,11 @@ interface OrderTableProps {
 }
 
 const OrderTable: React.FC<OrderTableProps> = ({ orders }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 8; // Defina o número de itens por página
+
+  const totalPages = Math.ceil(orders.length / itemsPerPage);
+
   const handleEditOrder = (orderId: number) => {
     console.log(`Editar pedido com ID ${orderId}`);
   };
@@ -15,9 +20,21 @@ const OrderTable: React.FC<OrderTableProps> = ({ orders }) => {
     console.log(`Excluir pedido com ID ${orderId}`);
   };
 
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, orders.length);
+  const currentOrders = orders.slice(startIndex, endIndex);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
+
   return (
-    <div className="container mx-auto p-8 bg-gray-100 rounded-3xl"> {/* Adicionando container */}
-      <div className="overflow-x-auto">
+    <div className="container mx-auto p-8 bg-gray-100 rounded-3xl">
+      <div className="overflow-x-auto relative">
         <table className="min-w-full divide-y divide-gray-200 rounded-lg overflow-hidden">
           <thead className="bg-gradient-to-r from-gray-600 to-gray-700 text-white">
             <tr>
@@ -30,7 +47,7 @@ const OrderTable: React.FC<OrderTableProps> = ({ orders }) => {
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {orders.map(order => (
+            {currentOrders.map(order => (
               <tr key={order.id} className="hover:bg-gray-100 transition-colors">
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border">{order.id}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border text-center">{order.orderId}</td>
@@ -49,6 +66,24 @@ const OrderTable: React.FC<OrderTableProps> = ({ orders }) => {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="mt-4 flex justify-between rounded-full">
+        {currentPage > 0 && (
+          <button
+            onClick={prevPage}
+            className="py-2 px-4 rounded-md bg-gray-700 hover:bg-slate-900 text-gray-200 mr-auto"
+          >
+            Anterior
+          </button>
+        )}
+        {currentPage < totalPages - 1 && (
+          <button
+            onClick={nextPage}
+            className="py-2 px-4 rounded-md bg-gray-700 hover:bg-slate-900 transition-colors text-gray-200 ml-auto"
+          >
+            Próximo
+          </button>
+        )}
       </div>
     </div>
   );
